152ddf209a076727ca2922222a968172f25c43c5
Jonathan <39104938+jord-baskov@users.noreply.github.com>
2019-08-24 17:58:16 -0300

    Update View.sh

--- View.sh
+++ View.sh
@@ -1,6 +1,7 @@
 #!/usr/bin env bash
 #Codado por Jord@rootExploit And LILITH AND STORM
 #Script para visualizar as senhas salvas em seu sistema
+#tenha o figlet instalado na sua maquina (sudo apt install figlet)
 figlet "ViewPass"
 echo "--# coded by jord@RootExploit #--"
 sleep 1
