023315c3e833cccd91803ef9284a672ca902fccc
Jonathan <39104938+jord-baskov@users.noreply.github.com>
2019-08-24 17:57:43 -0300

    Create View.sh

---
+++ View.sh
@@ -0,0 +1,23 @@
+#!/usr/bin env bash
+#Codado por Jord@rootExploit And LILITH AND STORM
+#Script para visualizar as senhas salvas em seu sistema
+figlet "ViewPass"
+echo "--# coded by jord@RootExploit #--"
+sleep 1
+if test `whoami` != root
+then
+echo ATENÇÃO
+echo Você precisa ser ROOT para executar este programa.
+exit 1
+fi
+echo 
+echo "esses são os arquivos de senhas salvas que temos em seu dir"
+echo "-----------------------------#"
+echo 
+ls /etc/NetworkManager/system-connections
+echo
+echo "-----------------------------#" 
+read -p "qual deseja abrir? " arquives
+echo
+#echo "senha :"
+cat  /etc/NetworkManager/system-connections/$arquives | grep 'psk='
