e642e74f29c22350c630f046428b1df5e3b07abd
Jonathan <39104938+jord-baskov@users.noreply.github.com>
2019-11-26 20:34:16 -0300

    Update View.sh

--- View.sh
+++ View.sh
@@ -1,15 +1,24 @@
 #!/usr/bin env bash
-#Codado por Jord@rootExploit And LILITH AND STORM
-#Script para visualizar as senhas salvas em seu sistema
-#tenha o figlet instalado na sua maquina (sudo apt install figlet)
-figlet "ViewPass"
+######################################################
+#Codado por jonathan Baskov(jord@rootExploit)
+#reatualizado em dia 26/11/2019
+#Blog: https://mindsetsecurity.wordpress.com
+#Salve: LILITH.NET,Sr Storm, LIRA, Kripton e geral ae
+######################################################
+echo "
+##     ## #### ######## ##      ## ########     ###     ######   ######  
+##     ##  ##  ##       ##  ##  ## ##     ##   ## ##   ##    ## ##    ## 
+##     ##  ##  ##       ##  ##  ## ##     ##  ##   ##  ##       ##       
+##     ##  ##  ######   ##  ##  ## ########  ##     ##  ######   ######  
+ ##   ##   ##  ##       ##  ##  ## ##        #########       ##       ## 
+  ## ##    ##  ##       ##  ##  ## ##        ##     ## ##    ## ##    ## 
+   ###    #### ########  ###  ###  ##        ##     ##  ######   ######  
+"
 echo "--# coded by jord@RootExploit #--"
 sleep 1
-if test `whoami` != root
-then
-echo ATENÇÃO
-echo Você precisa ser ROOT para executar este programa.
-exit 1
+if [ $(id -u) -eq 1000 ]; then
+	echo "Execute como ROOT!."
+	exit 1
 fi
 echo 
 echo "esses são os arquivos de senhas salvas que temos em seu dir"
@@ -20,5 +29,7 @@ echo
 echo "-----------------------------#" 
 read -p "qual deseja abrir? " arquives
 echo
-#echo "senha :"
+
 cat  /etc/NetworkManager/system-connections/$arquives | grep 'psk='
+sleep 3 
+echo "obrigado por usar meu script, acesse https://mindsetsecurity.wordpress.com/"
